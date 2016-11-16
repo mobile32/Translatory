822b67b91494a5c18ddc0e43df11f8be58013d4a
Grzegorz Zaręba <mobile443@gmail.com>
2016-11-16 23:26:08 +0100

    Zadnie 1 i 2

---
+++ Translatory/Parser.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Translatory
+{
+    public class Parser
+    {
+        public string Current { get; set; }
+        public int CurrentIndex { get; set; }
+        public List<string> Tokens { get; set; }
+
+        public void Parse(List<string> tokens)
+        {
+            Tokens = tokens;
+
+            Current = tokens.First();
+            CurrentIndex = 0;
+
+            W();
+        }
+
+        private void Next()
+        {
+            if (CurrentIndex + 1 < Tokens.Count)
+            {
+                CurrentIndex++;
+                Current = Tokens[CurrentIndex];
+            }
+        }
+        private void Accept(string expect)
+        {
+            if (expect == Current)
+            {
+                Next();
+            }
+            else
+            {
+                throw new System.ArgumentException("aktualny symbol rozni sie od oczekiwanego");
+            }
+        }
+
+        private void W()
+        {
+            S();
+            X();
+        }
+
+        private void S()
+        {
+            C();
+            Y();
+        }
+
+        private void C()
+        {
+            if (Current == "FLOAT")
+            {
+                Accept("FLOAT");
+            }
+            else if (Current == "INT")
+            {
+                Accept("INT");
+            }
+            else if (Current == "SYMBOL")
+            {
+                Accept("SYMBOL");
+
+            }
+            else
+            {
+                Accept("LEFT");
+                W();
+                Accept("RIGHT");
+            }
+        }
+
+        private void X()
+        {
+            if (Current == "OPERATOR")
+            {
+                Accept("OPERATOR");
+                S();
+                X();
+            }
+            else if (Current == "OPERATOR")
+            {
+                Accept("OPERATOR");
+                S();
+                X();
+            }
+        }
+
+        private void Y()
+        {
+            if (Current == "OPERATOR")
+            {
+                Accept("OPERATOR");
+                C();
+                Y();
+            }
+            else if (Current == "OPERATOR")
+            {
+                Accept("OPERATOR");
+                C();
+                Y();
+            }
+        }
+    }
+}
---
+++ Translatory/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Translatory
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var line = Console.ReadLine();
+
+            var defs = new[]
+            {
+                new TokenDefinition(@"\d*\.\d+([eE][-+]?\d+)?", "FLOAT"),
+                new TokenDefinition(@"\d+", "INT"),
+                new TokenDefinition(@"[\*\/\-\+]", "OPERATOR"),
+                new TokenDefinition(@"\(", "LEFT"),
+                new TokenDefinition(@"\)", "RIGHT"),
+                new TokenDefinition(@"\s", "SPACE"),
+                new TokenDefinition(@"\t", "TAB"),
+                new TokenDefinition(@"\n", "NEW LINE")
+            };
+
+            TextReader stringReader = new StringReader(line);
+            Lexer lexer = new Lexer(stringReader, defs);
+            List<string> tokens = new List<string>();
+
+            while (lexer.Next())
+            {
+                Console.WriteLine("Token: {0} Contents: {1}", lexer.Token, lexer.TokenContents);
+
+                if (lexer.Token.ToString() == "SPACE" || lexer.Token.ToString() == "TAB" ||
+                    lexer.Token.ToString() == "NEW LINE")
+                    continue;
+
+                tokens.Add(lexer.Token.ToString());
+            }
+
+            Console.WriteLine("Lekser skończył pracę");
+
+            Parser parser = new Parser();
+
+            if (tokens.Count > 0)
+            {
+                parser.Parse(tokens);
+                Console.WriteLine("Parsowanie zakończone sukcesem");
+            }
+        }
+    }
+}
